1ef0445774493799f6efbeffa843f9bdbdb4eeee
Owolabi Pius <opius2007@gmail.com>
2023-10-10 15:50:43 +0100

    minimum operations files

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
 
+""" Minimum Operations """
+
+
 def minOperations(n):
-    # If n is less than 2, it's impossible to achieve.
-    if n < 2:
+    """
+    In a text file, there is a single character H. Your text editor can execute
+    only two operations in this file: Copy All and Paste. Given a number n,
+    write a method that calculates the fewest number of operations needed to
+    result in exactly n H characters in the file.
+    Returns an integer
+    If n is impossible to achieve, returns 0
+    """
+    if not isinstance(n, int):
         return 0
 
-    # Initialize an array to store the minimum number of operations needed for each position.
-    dp = [0] * (n + 1)
-
-    # Loop through each position from 2 to n.
-    for i in range(2, n + 1):
-        dp[i] = i  # Initialize with the maximum possible operations.
-        for j in range(2, i):
-            if i % j == 0:
-                # If i is divisible by j, it means we can copy j characters and paste (i // j - 1) times.
-                dp[i] = min(dp[i], dp[j] + (i // j))
-
-    return dp[n]
-
-# Example usage:
-n = 9
-result = minOperations(n)
-print("Number of operations:", result)
+    op = 0
+    i = 2
+    while (i <= n):
+        if not (n % i):
+            n = int(n / i)
+            op += i
+            i = 1
+        i += 1
+    return op
