ff35c95c971c0ea96248b25fefd20076e0bbd4b7
Owolabi Pius <opius2007@gmail.com>
2023-11-07 15:51:55 +0100

    starwars_characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,29 +3,34 @@
  * Prints all characters of a Star Wars movie
  * The first positional argument passed is the Movie ID
  * Display one character name per line in the same order
- * as  list in the /films/ endpoint
+ * as the list in the /films/ endpoint
  */
 
 const request = require('request');
 const filmNum = process.argv[2] + '/';
 const filmURL = 'https://swapi-api.hbtn.io/api/films/';
-// Makes API request, sets async to allow await promise
+
+// Make an API request to get the film details
 request(filmURL + filmNum, async (err, res, body) => {
   if (err) return console.error(err);
 
-  // find URLs of each character in the film as a list obj
+  // Find URLs of each character in the film as a list obj
   const charURLList = JSON.parse(body).characters;
 
   // Use URL list to character pages to make new requests
-  // await queues requests until they resolve in order
   for (const charURL of charURLList) {
     await new Promise((resolve, reject) => {
       request(charURL, (err, res, body) => {
         if (err) return console.error(err);
 
-        // finds each character name and prints in URL order
-        console.log(JSON.parse(body).name);
-        resolve();
+        // Find each character name
+        request(JSON.parse(body).name, (err, res, body) => {
+          if (err) return console.error(err);
+
+          // Print the character name
+          console.log(body);
+          resolve();
+        });
       });
     });
   }
