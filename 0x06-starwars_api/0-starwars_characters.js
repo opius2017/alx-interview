79cf15544f7c5c2dd1cfa0cc5e8000f01c68ea02
Owolabi Pius <opius2007@gmail.com>
2023-11-07 15:57:32 +0100

    starwars_characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,41 +3,30 @@
  * Prints all characters of a Star Wars movie
  * The first positional argument passed is the Movie ID
  * Display one character name per line in the same order
- * as the list in the /films/ endpoint
+ * as  list in the /films/ endpoint
  */
 
 const request = require('request');
 const filmNum = process.argv[2] + '/';
 const filmURL = 'https://swapi-api.hbtn.io/api/films/';
-
-// Make an API request to get the film details
+// Makes API request, sets async to allow await promise
 request(filmURL + filmNum, async (err, res, body) => {
   if (err) return console.error(err);
 
-  // Find URLs of each character in the film as a list obj
+  // find URLs of each character in the film as a list obj
   const charURLList = JSON.parse(body).characters;
 
-  // Function to make requests in order
-  const getCharacter = (charURL) => {
-    request(charURL, (err, res, body) => {
-      if (err) return console.error(err);
+  // Use URL list to character pages to make new requests
+  // await queues requests until they resolve in order
+  for (const charURL of charURLList) {
+    await new Promise((resolve, reject) => {
+      request(charURL, (err, res, body) => {
+        if (err) return console.error(err);
 
-      // Find each character name
-      const character = JSON.parse(body);
-      console.log(character.name);
-
-      // Move to the next character, if available
-      const nextCharacterURL = charURLList.shift();
-      if (nextCharacterURL) {
-        getCharacter(nextCharacterURL);
-      }
+        // finds each character name and prints in URL order
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
     });
-  };
-
-  // Start the process with the first character
-  const firstCharacterURL = charURLList.shift();
-  if (firstCharacterURL) {
-    getCharacter(firstCharacterURL);
   }
 });
-
